d4e6e8e6e9317f9920f4ffcab02c35d3a32a18df
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-04-07 14:17:44 +0530

    React.Fragment and Portals

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UserList from "./components/Users/UserList";
 
@@ -10,10 +10,10 @@ function App() {
     })
   }
   return (
-    <div className="App">
+    <React.Fragment>
       <AddUser onAddUser = {addUserHander}/>
       <UserList users ={usersList}/>
-    </div>
+    </React.Fragment>
   );
 }
 
